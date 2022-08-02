13624cef3a7013a66978456304c248ac53cea4ea
zigzagzone <zigzagzone@gmail.com>
2022-08-02 21:38:32 +0700

    Update order page when no orders state

--- src/pages/Order.js
+++ src/pages/Order.js
@@ -3,41 +3,54 @@ import React, { useState, useEffect } from "react";
 export const Order = ({ orders }) => {
   const [tableId, setTableId] = useState(0);
 
+  const isNoOrders = orders.length === 0
+  const isNoOrdersAtSelectedTableId = orders.filter((order) => tableId === order.tableId).length === 0
+
   return (
     <main className="relative">
       <div className="px-4">
         <h1 className="text-3xl text-center mt-4">รายการสั่งอาหาร</h1>
         <div className="grid grid-cols-4 grid-flow-row gap-4 my-4">
           {[...Array(20).keys()].map((id) => (
-            <div
+            <button
               key={id}
-              onClick={() => setTableId(id + 1)}
-              className="bg-red-200 active:bg-red-300 text-center py-2 rounded-lg"
+              onClick={e => { 
+                if (id + 1 === tableId) {
+                  setTableId(0)
+                  e.target.blur()
+                } else {
+                  setTableId(id + 1)
+                }
+              }}
+              className="bg-red-200 active:bg-red-300 text-center py-2 rounded-lg focus:ring focus:ring-red-800"
             >
               {id + 1}
-            </div>
+            </button>
           ))}
         </div>
-        {orders
-          .filter((order) => tableId === 0 || tableId === order.tableId)
-          .map((order) => (
-            <div key={order.orderId} className="py-2 text-sm">
-              <div>หมายเลขคำสั่ง #{order.orderId}</div>
-              <div>โต๊ะ: {order.tableId}</div>
-              <div>สถานะ: {order.status}</div>
-              {order.items.map((item) => (
-                <div key={item.menuId} className="flex">
-                  <div className="flex-auto">{item.name}</div>
-                  <div>
-                    ฿{item.price} x {item.quantity}
+        {
+          isNoOrders ? <div>ไม่มีการสั่งอาหารทุกโต๊ะ</div> :
+          tableId !== 0 && isNoOrdersAtSelectedTableId ? <div>ไม่มีการสั่งอาหารที่โต๊ะ {tableId}</div> :
+          orders
+            .filter((order) => tableId === 0 || tableId === order.tableId)
+            .map((order) => (
+              <div key={order.orderId} className="py-2 text-sm">
+                <div>หมายเลขคำสั่ง #{order.orderId}</div>
+                <div>โต๊ะ: {order.tableId}</div>
+                <div>สถานะ: {order.status}</div>
+                {order.items.map((item) => (
+                  <div key={item.menuId} className="flex">
+                    <div className="flex-auto">{item.name}</div>
+                    <div>
+                      ฿{item.price} x {item.quantity}
+                    </div>
                   </div>
+                ))}
+                <div className="text-right">
+                  รวม ฿{order.totalPrice}
                 </div>
-              ))}
-              <div className="text-right">
-                รวม ฿{order.totalPrice}
               </div>
-            </div>
-          ))}
+            ))}
       </div>
     </main>
   );
